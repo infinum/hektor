eb0fb8c90623245ae80b0a016aa06ffb0712f621
Darko Kukovec <darko.kukovec@infinum.hr>
2015-05-09 20:37:40 +0200

    some cleanup

--- examples/readme.md
+++ examples/readme.md
@@ -1,4 +1,4 @@
 # Grunt
 1. Run npm install
-2. Make sure the hektor version in node_modules is the same version (and branch) as this example
+2. Make sure the HEKTOR version in node_modules is the same version (and branch) as this example
 3. Run one of the following grunt tasks: notify:hw, notify:foo, open
--- grunt/utils/task-wrap.js
+++ grunt/utils/task-wrap.js
@@ -9,20 +9,26 @@ module.exports = function(taskName, originalTask, transformer) {
       // Function that will transform our options into options that the wrapped task accepts
       transformer = transformer || function(options) { return options; };
 
-      // Wrapped task name
+      // Rename the loaded task with our prefix
       var wrappedTaskName = '_hektor_' + taskOptions.name;
-
-      // Rename the loaded task
       grunt.task.renameTask(taskOptions.name, wrappedTaskName);
 
       // Transform the options
-      grunt.config.set(wrappedTaskName, transformer(grunt.config.get(taskName)));
+      // getRaw is used in order to keep the <% %> templates
+      grunt.config.set(wrappedTaskName, transformer(grunt.config.getRaw(taskName)));
+
+      // Get the task description
+      var wrappedDescription;
+      try {
+        // This is an undocumented grunt feature. That's why it's inside of a try-catch block
+        wrappedDescription = grunt.task._tasks[wrappedTaskName].info;
+      } catch(e) {}
+      var description = taskOptions.description || wrappedDescription;
 
       // Register our wrapper task
-      var description = grunt.task._tasks[wrappedTaskName].info;
       grunt.registerTask(taskName, description, function(target) {
 
-        // Call the wrapped task
+        // Call the wrapped task, take care of the target
         var taskCall = target ? (wrappedTaskName + ':' + target) : wrappedTaskName;
         grunt.task.run(taskCall);
       });
