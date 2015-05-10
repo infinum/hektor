8901e894be80e99dfd16da0364c11f7638afcc95
Darko Kukovec <darko.kukovec@infinum.hr>
2015-05-10 11:55:32 +0200

    code comments for gulp stuff

--- gulp/main.js
+++ gulp/main.js
@@ -1,23 +1,31 @@
 var path = require('path');
 var _ = require('lodash');
 
+// Load the HEKTOR gulp dependencies
 var H = require('gulp-load-plugins')({
+  // We need to set the package.json path manually or it will take the project package, and we don't want that
   config: path.normalize(__dirname + '../../package.json')
 });
 
 module.exports = function(gulp) {
 
   function loadModules(modules) {
+    // Transform the argument into an object where keys are task names and values are task options
+    modules = modules || {};
     if (typeof modules === 'string') {
       modules = [].concat(modules);
     }
     if (modules instanceof Array) {
       modules = _.zipObject(modules);
     }
+
     _.each(modules, function(options, module) {
       if (H[module]) {
+        // Make sure we don't overwrite a dependency task
         H['_hektor_' + module] = H[module];
       }
+
+      // sass task is also documented
       H[module] = require('./tasks/' + module)(gulp, H, options);
     });
     return H;
--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,6 +1,15 @@
 module.exports = function(gulp, H, options) {
   options = options || false;
+
+  // Create a standard gulp task
   return gulp.task('sass', function() {
+
+    // By default, tasks run with maximum concurrency (https://github.com/gulpjs/gulp/blob/master/docs/API.md#async-task-support)
+    // That's why a task should _ALWAYS_ do one of the following:
+    // * return a stream
+    // * return a promise
+    // * call the callback function (the only argument in the task function)
+    //   callback should get (null|undefined) as only argument if all OK, or an error object
     return gulp.src(options.src || 'app/styles/main.scss')
       .pipe(H.plumber({
         errorHandler: H.notify.onError('Sass: <%= error.message %>')
